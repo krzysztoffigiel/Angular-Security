a1d16a11607247420f5f0fcdb4db9d14f57adc0b
krzysztoffigiel <krzysztoffigiel@MacBook-Pro-Krzysztof-2.local>
2018-07-29 13:30:33 +0200

    server.ts implementation

--- angular-security/server/server.ts
+++ angular-security/server/server.ts
@@ -1,5 +1,10 @@
 import * as express from 'express';
-import {Application} from 'express';
+import { Application } from 'express';
+import * as fs from 'fs';
+import * as https from 'https';
+import { readAllBooks } from './read-all-books.route';
+import * as _ from 'lodash';
+
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -17,3 +22,25 @@ const optionDefinitions = [
 const options = commandLineArgs(optionDefinitions);
 
 // REST API
+
+app.route('/api/books').get(readAllBooks);
+
+if (options.secure) {
+
+    const httpsServer = https.createServer({
+        key: fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem')
+    }, app);
+
+    // launch an HTTPS Server. Note: this does NOT mean that the application is secure
+    httpsServer.listen(9000, () => console.log("HTTPS Secure Server running at https://localhost:" + httpsServer.address().port));
+
+}
+else {
+
+    // launch an HTTP Server
+    const httpServer = app.listen(9000, () => {
+        console.log("HTTP Server running at https://localhost:" + httpServer.address().port);
+    });
+
+}
