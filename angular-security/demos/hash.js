4ee72d9c2a7813de32d740cfe723cfc07841fd95
krzysztoffigiel <krzysztoffigiel@MacBook-Pro-Krzysztof-2.local>
2018-08-10 23:26:16 +0200

    password salting

--- angular-security/demos/hash.js
+++ angular-security/demos/hash.js
@@ -1,8 +1,16 @@
 
 var crypto = require('crypto');
 
-var password = 'password';
+var password = '123456789';
 
-var hash = crypto.createHash('sha256').update(password).digest('hex');
+// for more efficient cryptography attacks prevent
+
+var aliceSalt = 1;
+
+var bobSalt = 2;
+
+
+
+var hash = crypto.createHash('sha256').update(password + aliceSalt).digest('hex');
 
 console.log("Result of hashing function for password " + "'" + password + "'" + " is \n\n" + hash + "\n\n");
