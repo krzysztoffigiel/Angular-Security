5938b716d50edfb2e5648e17e2754c705ab4f12a
krzysztoffigiel <krzysztoffigiel@MacBook-Pro-Krzysztof-2.local>
2018-08-11 13:21:06 +0200

    hash salting method with PBDKF2

--- angular-security/demos/hash.js
+++ angular-security/demos/hash.js
@@ -9,8 +9,8 @@ var aliceSalt = 1;
 
 var bobSalt = 2;
 
+// I can add salt to the password in update method and get another results for each user that have the same password
 
-
-var hash = crypto.createHash('sha256').update(password + aliceSalt).digest('hex');
+var hash = crypto.createHash('sha256').update(password).digest('hex');
 
 console.log("Result of hashing function for password " + "'" + password + "'" + " is \n\n" + hash + "\n\n");
