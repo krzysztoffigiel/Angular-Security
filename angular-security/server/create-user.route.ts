141e4850dd995fadd1e7b7326e334a1212f1387b
fj2016 <kfigiel.projekty@gmail.com>
2018-08-25 11:59:28 +0200

    create-user.route file token add

--- angular-security/server/create-user.route.ts
+++ angular-security/server/create-user.route.ts
@@ -13,16 +13,23 @@ export function createUser(req: Request, res: Response) {
     if (errors.length > 0) {
         res.status(400).json({ errors });
     } else {
-        argon2.hash(credentials.password).then(passwordDigest => {
-            const user = db.createUser(credentials.email, passwordDigest);
-
-            console.log(USERS);
-
-            res.status(200).json({ id: user.id, email: user.email });
-        });
+        createUserAndSession(res, credentials);
     }
 
+    // tslint:disable-next-line:no-shadowed-variable
+    async function createUserAndSession(res: Response, credentials) {
+        // tslint:disable-next-line:no-shadowed-variable
+        const passwordDigest = await argon2.hash(credentials.password);
 
+        const user = db.createUser(credentials.email, passwordDigest);
+
+        const sessionToken = 1;
+
+        res.cookie('SESSIONID', sessionToken, {httpOnly: true, secure: true});
+
+        res.status(200).json({id: user.id, email: user.email});
+
+    }
 
 
 
--- angular-security/server/logout.route.ts
+++ angular-security/server/logout.route.ts
@@ -4,7 +4,7 @@ import {Request, Response} from 'express';
 
 export function logout(req: Request, res: Response) {
 
-    res.clearCookie("SESSIONID");
+    res.clearCookie('SESSIONID');
 
     res.status(200).json({message: 'Logout Successful'});
 }
